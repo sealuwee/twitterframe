b3ccea1c6e9c1d79dd34933ef6a2a64d5fdbce25
Ruwai <chrisl6uie@gmail.com>
2019-03-19 19:53:01 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+twitterframe
+- a command line application to create dataframes from Twitter data.
+- must have Twitter developer keys.
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "sklearn",
+    "seaborn",
+    "scipy",
+    "matplotlib.pyplot"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-ruwai",
+    version="0.0.4",
+    author="Ruwai",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ruwai/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
