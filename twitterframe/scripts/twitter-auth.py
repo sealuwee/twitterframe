f6d9fb6af6d677799c36242c42b71287fd047527
Ruwai <chrisl6uie@gmail.com>
2019-03-21 14:29:24 -0700

    added new files.

--- twitterframe/scripts/twitter-auth.py
+++ twitterframe/scripts/twitter-auth.py
@@ -52,4 +52,5 @@ app_name, access_token, access_token_secret, consumer_key, consumer_secret = che
 time.sleep(2)
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
 auth.set_access_token(access_token, access_token_secret)
