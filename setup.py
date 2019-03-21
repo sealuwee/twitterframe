edc35b9800c0a8b5e2c856514c7cdd3e9101f762
Ruwai <chrisl6uie@gmail.com>
2019-03-21 01:02:50 -0700

    wait actually this is the real commit with my changes for the dependencies.

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ REQUIRED = [
     "twitter",
     "tweepy",
     "pandas",
-    "sklearn"
+    "click"
 ]
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
+
     name="twitterframe",
     version="0.0.1",
     author="Ruwai",
@@ -28,7 +29,9 @@ setuptools.setup(
     python_requires=">=3.5",
     install_requires=REQUIRED,
     classifiers=[
+
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+
     ],)
