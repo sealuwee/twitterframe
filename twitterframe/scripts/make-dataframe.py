a7b40bc8a471dbebb50dfdb09f2700f20efce750
Ruwai <chrisl6uie@gmail.com>
2019-03-21 20:24:55 -0700

    added changes to make-dataframe.py

--- twitterframe/scripts/make-dataframe.py
+++ twitterframe/scripts/make-dataframe.py
@@ -10,3 +10,45 @@ import click
 import csv
 
 twitter_emoji = config.twitter_emoji
+
+def get_tweets(username):
+
+    '''
+        Code redesigned from yanofsky:
+        https://gist.github.com/yanofsky/5436496
+        and the Python 3.x version:
+        https://gist.github.com/gabrielsoule/638201ac0cc12828d3cde69035a25336
+    '''
+
+    api = config.api
+
+    tweets = []
+
+    new_tweets = api.user_timeline(sceen_name=username, count=200)
+
+    tweets.extend(new_tweets)
+
+    oldest_tweets = tweets[-1].id - 1
+
+    while len(new_tweets) > 0:
+
+        print(twitter_emoji, 'Getting tweets before %s' % (oldest_tweets))
+
+        new_tweets = api.user_timeline(screen_name=username, count=200,max_id=oldest_tweets)
+
+        tweets.extend(new_tweets)
+
+        oldest_tweets = tweets[-1].id - 1
+
+        print(twitter_emoji, '... %s Amount of tweets downloaded so far' % (len(tweets)))
+
+    output_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
+
+    with open('%s_tweets.csv' % username, 'w') as tw:
+
+        writer = csv.writer(tw)
+        writer.writerow(['id', 'created_at', 'text'])
+        writer.writerows(output_tweets)
+
+    pass
+
