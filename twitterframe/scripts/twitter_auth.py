cba1d6b37432f1f21f02e31eee512e7c37cfff83
Ruwai <chrisl6uie@gmail.com>
2019-03-24 12:37:14 -0700

    made a minor change

--- twitterframe/scripts/twitter_auth.py
+++ twitterframe/scripts/twitter_auth.py
@@ -42,11 +42,12 @@ class SetupAPI(object):
 
         filename = Path(filename)
         if '.json' not in filename.parts[-1]:
-            print(check, 'Made file path as .json')
+            print(check, 'Made file path as .json',check)
             filename = filename.joinpath(filename.parts[-1]+'.json')
         tw = open(filename.name, 'w+')
         tw.write(json.dumps(keys))
         tw.close()
+        print(check,'Keys have been stored in: ',filename,check)
 
     def setup(self):
 
