cc591f0707eedc10a86356123acd37ad611f7c59
Ruwai <chrisl6uie@gmail.com>
2019-03-29 18:40:51 -0400

    modified some stuff

--- twitterframe/scripts/twitter_listener.py
+++ twitterframe/scripts/twitter_listener.py
@@ -3,6 +3,8 @@
 	Might make it more efficient, who knows!
 '''
 
+from . import twitter_methods
+from . import utils
 import tweepy
 import os
 import pathlib as Path 
@@ -20,3 +22,43 @@ class TwitterListener(tweepy.StreamListener):
     def on_status(self, status):
 
         print(status.text)
+
+        	def on_error(self, status_code):
+
+        if status_code == 420:
+
+        	return False
+
+    def set_endpoint(self, endpoint):
+    	'''
+    		Sets endpoints
+    	'''
+    	self.endpoint = endpoint
+
+   	def on_data(self, raw_data):
+   		'''
+   			When data arrives to a method that dispatches the event to the right person.
+   		'''
+   		data = json.loads(raw_data)
+
+   		if 'direct_message' in data:
+   			direct_message = data['direct_message']
+   			if direct_message['sender']['id'] != self.endpoint._api.me().id:
+   				return self.endpoint.process_new_direct_message(direct_message)
+
+   		elif data.get('event','') == 'follow':
+   			return self.endpoint.process_new_follower(data['source'])
+
+   	def use_filter(self, tracker):
+   		'''
+   			Filter to track what you want to stream.
+   		'''
+   		api = self.setup()
+
+   		stream = tweepy.Stream(auth=api, listener=)
+class StreamListener(tweepy.StreamListener):
+	'''
+		Stream Listener class. 
+	'''
+
+
