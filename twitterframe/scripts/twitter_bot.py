605cf38b8837630ea30cd3bc1b54c274b251b88b
Ruwai <chrisl6uie@gmail.com>
2019-04-02 18:02:03 -0400

    added some stuff to the bot script, doesnt function quite yet!

--- twitterframe/scripts/twitter_bot.py
+++ twitterframe/scripts/twitter_bot.py
@@ -27,9 +27,31 @@ class RobotActions:
 
 		return self.api
 
+	def get_followers(self, account_name):
+        """Return a list of all the followers of an account"""
+        followers = []
+        for page in tweepy.Cursor(self.api.followers_ids, screen_name=str(account_name)).pages():
+            followers.extend(page)
+        return followers
+
+    def get_user_from_tweet(self, tweet_id):
+        """Return an User object from a given tweet"""
+        tweet = self.get_tweet(tweet_id)
+        return tweet.user
+
+    def get_tweet(self, tweet_id):
+        """Return a Status object from a tweet ID"""
+        return self.api.get_status(tweet_id)
+
 	def new_tweets(self, tweet):
 		'''
 			Publish a new tweet
 		'''
-
 		self.api.update_status(str(tweet))
+
+	def follow_account(self, user_id):
+
+		if not self.api.get_user(user_id) in self.followers:
+			self.api.create_friendship(user_id)
+
+	
