189f8bb5cfb844f7d45f3dcb3e3a607c98df571b
Ruwai <chrisl6uie@gmail.com>
2019-04-07 00:11:32 -0700

    added twitter_cmd.py

---
+++ twitterframe/scripts/twitter_cmd.py
@@ -0,0 +1,21 @@
+from cmd import Cmd
+from . import utils
+import tweepy
+import json
+import csv
+import os
+from pathlib import Path
+import time
+from tqdm import tqdm
+import psychopg2
+
+class TwitterFrame(Cmd):
+    '''
+        will basically be like SQL queries
+        with some added stuff like creating databases,tables, etc.
+    '''
+    def do_createdb(self, args):
+        '''
+            CREATE DATABASE name;
+        '''
+        pass
