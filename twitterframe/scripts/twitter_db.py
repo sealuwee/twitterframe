ad1fd8edef67bf416747611a744913c814689f07
Ruwai <chrisl6uie@gmail.com>
2019-04-06 21:10:43 -0700

    began working on twitter_db.py

---
+++ twitterframe/scripts/twitter_db.py
@@ -0,0 +1,17 @@
+'''
+twitter_db file to house the class method for working with
+tweepy._json files and adding them to a database of choice.
+
+twitter_db will be the first script I use with the new shell in twitter_cmd
+and will only be accessible from the shell environment.
+'''
+
+from . import utils
+import tweepy
+import json
+import csv
+import os
+from pathlib import Path
+import time
+from tqdm import tqdm
+import psychopg2
